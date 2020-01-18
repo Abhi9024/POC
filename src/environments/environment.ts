7c7e2ce7a4da9d6322b7fd00be4ec8dfabb338e9
Abhi9024 <gr.abishek.gr@gmail.com>
2020-01-18 15:09:12 +0530

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,6 @@
 
 export const environment = {
   production: false,
-  APIURL: "http://w1d01-qrweb01.mediconnect.net:8600/api"
+  //APIURL: "http://w1d01-qrweb01.mediconnect.net:8600/api",
+  APIURL: "http://ec2-13-233-113-94.ap-south-1.compute.amazonaws.com/api";
 };
